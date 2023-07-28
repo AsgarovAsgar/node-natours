02f83e6bf5648e6e916d4c615117507f54192728
Asgarov Asgar <asgara@maestro.st>
2023-07-28 16:14:28 +0400

    refactor for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,20 +5,6 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-const checkID = (req, res, next, val) => {
-  let id = val
-  const tour = tours.find((t) => t.id === +id);
-  console.log(`Tour id is ${val}`);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next()
-}
-
 const checkBody = ((req, res, next) => {
   if(!(req.body.hasOwnProperty('name') && req.body.hasOwnProperty('price'))) {
     return res.status(400).json({
@@ -32,57 +18,41 @@ const checkBody = ((req, res, next) => {
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
-    results: tours.length,
-    data: { tours },
+    // results: tours.length,
+    // data: { tours },
   });
 };
 
 const getTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find((t) => t.id === +id);
 
   res.status(200).json({
     status: "success",
-    data: { tour },
+    // data: { tour },
   });
 };
 
 const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: newTour,
-      });
-    }
-  );
+  res.status(201).json({
+    status: "success",
+    // data: newTour,
+  });
 };
 
 const updateTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find((t) => t.id === +id);
-
   res.status(200).json({
     status: "success",
-    data: { tour: "Updated tour here" },
+    // data: { tour: "Updated tour here" },
   });
 };
 
 const deleteTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find((t) => t.id === +id);
+  // const { id } = req.params;
+  // const tour = tours.find((t) => t.id === +id);
 
   res.status(204).json({
     status: "success",
-    data: null,
+    // data: null,
   });
 };
 
-module.exports = { checkID, checkBody, getAllTours, getTour, createTour, updateTour, deleteTour }
+module.exports = { checkBody, getAllTours, getTour, createTour, updateTour, deleteTour }
---
+++ models/tourModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    default: 4.5
+  },
+  rating: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour', tourSchema) // this is a model
+module.exports = Tour
