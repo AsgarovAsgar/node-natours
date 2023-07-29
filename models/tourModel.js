6b83722f945019e103a5b8da3bf0b1c14bd07265
Asgarov Asgar <asgara@maestro.st>
2023-07-29 23:26:58 +0400

    imported dev data using command

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,55 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  price: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
   },
-  rating: {
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
     type: Number,
     required: [true, 'A tour must have a price']
-  }
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema) // this is a model
