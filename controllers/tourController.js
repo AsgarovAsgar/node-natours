dabfeabbbc5ee8d91ce346a026ffab31d73a6531
Asgarov Asgar <asgara@maestro.st>
2023-07-30 21:04:32 +0400

    refactored API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel')
+const APIFeatures = require('../utils/apiFeatures')
 
 
 const aliasTopTours = (req, res, next) => {
@@ -18,55 +19,18 @@ const aliasTopTours = (req, res, next) => {
 //   next()
 // })
 
+
+
 const getAllTours = async (req, res) => {
   try {
-    // console.log(req.query)
-
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query }
-    const excludedFields = ['page', 'sort', 'limit', 'fields']
-    excludedFields.forEach(el => delete queryObj[el])
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|lte|gt|lt)\b/g, match => `$${match}`)
-    // console.log(JSON.parse(queryStr));
-    
-    
-    // const query = Tour.find(queryObj)
-    let query = Tour.find(JSON.parse(queryStr))
-
-    // 2) Sorting
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      query = query.sort(sortBy)
-    } else {
-      query = query.sort('-createdAt')
-    }
-
-    // 3) Field limiting
-    if(req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v')
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1
-    const limit = req.query.limit * 1 || 100
-    const skip = (page - 1) * limit
-
-    query = query.skip(skip).limit(limit)
-
-    if(req.query.page) {
-      const numTours = await Tour.countDocuments()
-      if(skip >= numTours) throw new Error('This page does not exist')
-    }
-
     // EXECUTE QUERY
-    const tours = await query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+
+    const tours = await features.query
 
     // const query = Tour.find()
     //   .where('duration')
