6d41a36eb33cfcdb4f092a4301f468e533e55997
Asgarov Asgar <asgara@maestro.st>
2023-07-31 23:59:18 +0400

    added aggs

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { aliasTopTours, getAllTours, getTour, createTour, updateTour, deleteTour } = require('./../controllers/tourController')
+const { aliasTopTours, getAllTours, getTour, createTour, updateTour, deleteTour, getTourStats, getMonthlyPlan } = require('./../controllers/tourController')
 
 const router = express.Router()
 
 // router.param('id')
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
+router.route('/tour-stats').get(getTourStats)
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 
 router
   .route("/")
