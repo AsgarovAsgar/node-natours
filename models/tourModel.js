9b4598f0e1f1127db0f10f71ac4becd7ac7a9e5f
Asgarov Asgar <asgara@maestro.st>
2023-08-05 23:14:43 +0400

    added custom validator

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
 
 const slugify = require('slugify')
+const validator = require('validator')
 
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxLength: [40, 'A tour name must have less or equal 40 characters'],
+    minLength: [10, 'A tour name must have more or equal 10 characters']
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
   },
   slug: String,
   duration: {
@@ -20,11 +24,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
   },
   ratingsQuantity: {
     type: Number,
@@ -36,6 +46,14 @@ const tourSchema = new mongoose.Schema({
   },
   priceDiscount: {
     type: Number,
+    validate: {
+      validator: function(val) {
+        // this only points to current doc on new doc creation
+        return val < this.price
+      },
+      message: 'Discount price ({VALUE}) should be below regular price'
+    }
+    
   },
   summary: {
     type: String,
