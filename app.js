7b7b98c5998a03831472d8855e575e20690c5573
Asgarov Asgar <asgara@maestro.st>
2023-08-06 23:33:16 +0400

    created error controller

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require('morgan')
 
 const app = express();
+
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
+
 const tourRouter = require(`${__dirname}/routes/tourRoutes`)
 const userRouter = require(`${__dirname}/routes/userRoutes`)
 
@@ -47,11 +51,20 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
-  })
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // })
+
+  // const err  = new Error(`Can't find ${req.originalUrl} on this server`)
+  // err.status = 'fail'
+  // err.statusCode = 404
+
+  // next(err) // always can pass err as the first argument
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404)) // always can pass err as the first argument
 })
+
+app.use(globalErrorHandler)
 
 // 4) START SERVER
 
