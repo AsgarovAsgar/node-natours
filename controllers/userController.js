fe28a224e28a4ff2b87681b15068a8dd54a78e9e
Asgarov Asgar <asgara@maestro.st>
2023-08-15 23:11:05 +0400

    added updateMe route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel')
 // const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 const getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find()
@@ -17,6 +18,19 @@ const createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'This route is not defined yet!'
+  })
+}
+
+const updateMe = (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if(req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400))
+  }
+
+  // 2) Update user document
+  res.status(200).json({
+    status: 'success',
+    message: 'update'
   })
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('./../controllers/userController')
-const { signup, login, protect, forgotPassword, resetPassword, updatePassword } = require('./../controllers/authController')
+const { getAllUsers, createUser, getUser, updateUser, deleteUser, updateMe } = require('./../controllers/userController')
+const { signup, login, protect, forgotPassword, resetPassword, updatePassword } = require('./../controllers/authController');
 
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.post('/login', login)
 router.post('/forgotPassword', forgotPassword)
 router.patch('/resetPassword/:token', resetPassword)
 router.patch('/updateMyPassword', protect, updatePassword)
+
+router.patch('/updateMe', protect, updateMe)
 
 router
   .route("/")
