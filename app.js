9e9637e101ea59e3026d5af278983217e8acc95f
Asgarov Asgar <asgara@maestro.st>
2023-08-16 12:22:36 +0400

    added mongo-sanitize, rate-limit, helmet, and hpp for security and data sanitization

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const app = express();
 
@@ -9,12 +14,39 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require(`${__dirname}/routes/tourRoutes`)
 const userRouter = require(`${__dirname}/routes/userRoutes`)
 
-// 1) MIDDLEWARE
+// 1) GLOBAL MIDDLEWARES
 
+// Set security HTTP covers
+app.use(helmet())
+
+// Development logging
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 660 * 60 * 1000,
+  message: 'Too may requests from this IP. Please try again in an hour'
+})
+app.use('/api', limiter)
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: [ 'duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price' ]
+}))
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
 // place middleware before route functions
@@ -23,26 +55,16 @@ app.use(express.static(`${__dirname}/public`))
 //   next()
 // })
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   next()
 })
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'Hello from server side', app: 'Natours'})
-})
-
-// app.post('/', (req, res) => {
-//   res.send('You can send ...')
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: 'Hello from server side', app: 'Natours'})
 // })
 
-// 2) Route handlers
-
-// app.get('/api/v1/tours', getAllTours)
-// app.get('/api/v1/tours/:id', getTour)
-// app.post('/api/v1/tours', createTour)
-// app.patch('/api/v1/tours/:id', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
 
 // 3) ROUTES
 
