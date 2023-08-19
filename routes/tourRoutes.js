6a9533dfdc78f75d0de6bbcfba307b02d1175407
Asgarov Asgar <asgara@maestro.st>
2023-08-20 03:39:01 +0400

    implemented nested route for reviews

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { aliasTopTours, getAllTours, getTour, createTour, updateTour, deleteTour, getTourStats, getMonthlyPlan } = require('./../controllers/tourController')
 const { protect, restrictTo } = require('./../controllers/authController')
+const reviewRouter = require('./reviewRoutes')
 
 const router = express.Router()
 
 // router.param('id')
+
+router.use('/:tourId/reviews', reviewRouter)
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
 router.route('/tour-stats').get(getTourStats)
@@ -22,5 +25,10 @@ router
   .patch(updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
   // .delete(deleteTour);
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(protect, restrictTo('user'), reviewController.createReview)
+
 
 module.exports = router
