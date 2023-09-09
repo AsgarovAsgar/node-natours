349f5c4ef9d6bf55ed980a0468fb218663c1c112
Asgarov Asgar <asgara@maestro.st>
2023-09-09 18:57:58 +0400

    processing multiple images

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { aliasTopTours, getAllTours, getTour, createTour, updateTour, deleteTour, getTourStats, getMonthlyPlan, getToursWithin, getDistances } = require('./../controllers/tourController')
+const { uploadTourImages, resizeTourImages, aliasTopTours, getAllTours, getTour, createTour, updateTour, deleteTour, getTourStats, getMonthlyPlan, getToursWithin, getDistances } = require('./../controllers/tourController')
 const { protect, restrictTo } = require('./../controllers/authController')
 const reviewRouter = require('./reviewRoutes')
 
@@ -27,7 +27,7 @@ router
 router
   .route("/:id")
   .get(getTour)
-  .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
+  .patch(protect, restrictTo('admin', 'lead-guide'), uploadTourImages, resizeTourImages, updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
   // .delete(deleteTour);
 
