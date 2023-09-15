e758770e35f36033481c4c1978becaf38f776b3f
Asgarov Asgar <asgara@maestro.st>
2023-09-15 13:00:36 +0400

    completed booking

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const bookingController = require('./../controllers/bookingController')
-const { protect } = require('./../controllers/authController')
+const { protect, restrictTo } = require('./../controllers/authController')
 
 const router = express.Router()
 
-router.get(
-  '/checkout-session/:tourId',
-  protect, 
-  bookingController.getCheckoutSession
-)
+router.use(protect)
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession)
+
+router.use(restrictTo('admin', 'lead-guide'))
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking)
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking)
+
 
 module.exports = router
