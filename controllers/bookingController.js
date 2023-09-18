16350f88fab6e7f13a46b317e38f634c8f9b2a2e
Asgarov Asgar <asgara@maestro.st>
2023-09-19 00:45:00 +0400

    fixed typo

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -64,7 +64,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id
-  const user = (await User.findOne({email: session.customer_email})).tourId
+  const user = (await User.findOne({email: session.customer_email})).id
   const price = session.amount_total / 100
 
   await Booking.create({ tour, user, price })
@@ -84,7 +84,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if(event === 'checkout.session.completed') createBookingCheckout(event.data.object)
+  if(event.type === 'checkout.session.completed') createBookingCheckout(event.data.object)
   res.status(200).json({
     received: true
   })
